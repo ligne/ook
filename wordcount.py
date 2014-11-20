1aa7c1801db600c2f5dd9b69c4128268b6f8d79c
Matthew Boyle <matt.boyle@gmail.com>
2014-11-20 13:55:05 +0000

    retab.

--- wordcount.py
+++ wordcount.py
@@ -21,7 +21,7 @@ DEVNULL = open(os.devnull, 'w')
 
 # open the collections
 with open(os.environ['HOME'] + '/.kindle/system/collections.json') as c:
-        coll = json.load(c)
+    coll = json.load(c)
 
 # cache word-counts
 wcs = shelve.open(os.environ['HOME'] + '/.wordcounts.pickle')
@@ -30,93 +30,93 @@ wcs = shelve.open(os.environ['HOME'] + '/.wordcounts.pickle')
 ### Functions and stuff
 
 def file_infos(path, ext):
-        if path not in wcs:
-                wcs[path] = wordcount(path)
-        words = wcs[path]
+    if path not in wcs:
+        wcs[path] = wordcount(path)
+    words = wcs[path]
 
-        author, title = metadata(path, ext)
+    author, title = metadata(path, ext)
 
-        return {
-                'title':  title,
-                'author': author,
-                'words': words,
-        }
+    return {
+        'title':  title,
+        'author': author,
+        'words':  words,
+    }
 
 
 def wordcount(path):
-        if subprocess.call(['ebook-convert', path, '/tmp/test.txt'], stdout=DEVNULL, stderr=DEVNULL):
-                return
+    if subprocess.call(['ebook-convert', path, '/tmp/test.txt'], stdout=DEVNULL, stderr=DEVNULL):
+        return
 
-        words = 0
-        with open('/tmp/test.txt', 'r') as book:
-                for line in book:
-                        words += len(line.split(None))
+    words = 0
+    with open('/tmp/test.txt', 'r') as book:
+        for line in book:
+            words += len(line.split(None))
 
-        return words
+    return words
 
 
 def metadata(path, ext):
-        # metatdata
-        stream = open(path, 'r+b')
+    # metatdata
+    stream = open(path, 'r+b')
 
-        try:
-                mi = get_metadata(stream, ext, force_read_metadata=True)
-        except:
-                return
+    try:
+        mi = get_metadata(stream, ext, force_read_metadata=True)
+    except:
+        return
 
-        title  = re.sub(r'\n', ' ', re.sub(r'^(the|a|le|la|les) ', '', mi.get('title'), flags=re.I).expandtabs())
-        author = re.sub(r'\n', ' ', mi.get('authors')[0])
+    title  = re.sub(r'\n', ' ', re.sub(r'^(the|a|le|la|les) ', '', mi.get('title'), flags=re.I).expandtabs())
+    author = re.sub(r'\n', ' ', mi.get('authors')[0])
 
-        return (author, title)
+    return (author, title)
 
 
 def print_entry(fi, filehandle=sys.stdout):
-        if fi and fi['words'] is not None:
-                fh.write('{words}\t{title}'.format(**fi))
-                if fi['author'] != 'Unknown':
-                        fh.write(' ({author})'.format(**fi))
-                fh.write('\n')
+    if fi and fi['words'] is not None:
+        fh.write('{words}\t{title}'.format(**fi))
+        if fi['author'] != 'Unknown':
+            fh.write(' ({author})'.format(**fi))
+        fh.write('\n')
 
 
 def collection_lookup(key, f):
-	ret = None
-        for n, c in coll.items():
-                if key in c['items']:
-                        cname = re.split(r'@', n, maxsplit=1)
-			if ret:
-				print "{} is in {} as well as {}".format(f, cname[0], ret)
-                        ret = cname[0]
-        return ret
+    ret = None
+    for n, c in coll.items():
+        if key in c['items']:
+            cname = re.split(r'@', n, maxsplit=1)
+            if ret:
+                print "{} is in {} as well as {}".format(f, cname[0], ret)
+            ret = cname[0]
+    return ret
 
 
 def get_collection(f):
-        # hash of the filename
-        HASH_PREFIX = "/mnt/us/documents/"
-        key = "*%s" % hashlib.sha1("/mnt/us/documents/%s" % os.path.basename(f)).hexdigest()
+    # hash of the filename
+    HASH_PREFIX = "/mnt/us/documents/"
+    key = "*%s" % hashlib.sha1("/mnt/us/documents/%s" % os.path.basename(f)).hexdigest()
+    c = collection_lookup(key, f)
+    if c:  return c
+
+    # documents sent via amazon.
+    asin = re.search(r'-asin_([A-Z0-9]+)-type_([A-Z]+)-', f)
+    if asin:
+        key = '#{}^{}'.format(*asin.groups())
+        c = collection_lookup(key, f)
+    if c:  return c
+
+    # embedded asin identifier
+    stream = open(f, 'r+b')
+    try: mi = get_metadata(stream, 'mobi', force_read_metadata=True)
+    except: pass
+    if mi and mi.has_identifier('mobi-asin'):
+        # EBOK suffix
+        key = '#{}^{}'.format(mi.get_identifiers()['mobi-asin'], 'EBOK')
         c = collection_lookup(key, f)
         if c:  return c
+        # PDOC suffix
+        key = '#{}^{}'.format(mi.get_identifiers()['mobi-asin'], 'PDOC')
+        c = collection_lookup(key, f)
 
-        # documents sent via amazon.
-        asin = re.search(r'-asin_([A-Z0-9]+)-type_([A-Z]+)-', f)
-        if asin:
-                key = '#{}^{}'.format(*asin.groups())
-                c = collection_lookup(key, f)
-        if c:  return c
-
-        # embedded asin identifier
-        stream = open(f, 'r+b')
-        try: mi = get_metadata(stream, 'mobi', force_read_metadata=True)
-        except: pass
-        if mi and mi.has_identifier('mobi-asin'):
-                # EBOK suffix
-                key = '#{}^{}'.format(mi.get_identifiers()['mobi-asin'], 'EBOK')
-                c = collection_lookup(key, f)
-                if c:  return c
-                # PDOC suffix
-                key = '#{}^{}'.format(mi.get_identifiers()['mobi-asin'], 'PDOC')
-                c = collection_lookup(key, f)
-
-        return c
+    return c
 
 
 # main
@@ -124,31 +124,31 @@ def get_collection(f):
 d = tempfile.mkdtemp() + '/'
 
 fhs = {
-        'later':          open(d + '/article-lengths-later.txt', 'w'),
-        'books':          open(d + '/book-lengths-later.txt', 'w'),
-        'short stories':  open(d + '/book-lengths-shortstories.txt', 'w'),
+    'later':          open(d + '/article-lengths-later.txt', 'w'),
+    'books':          open(d + '/book-lengths-later.txt', 'w'),
+    'short stories':  open(d + '/book-lengths-shortstories.txt', 'w'),
 }
 
 # books
 fh_no = open(d + '/book-lengths.txt', 'w')
 for ext in 'prc', 'mobi', 'txt':
-        for f in glob.glob(os.environ['HOME'] + '/.kindle/documents/*.' + ext):
-                c = get_collection(f)
-                fh = fhs.get(c, fh_no)
-                print_entry(file_infos(f, 'mobi'), fh)
+    for f in glob.glob(os.environ['HOME'] + '/.kindle/documents/*.' + ext):
+        c = get_collection(f)
+        fh = fhs.get(c, fh_no)
+        print_entry(file_infos(f, 'mobi'), fh)
 fh_no.close()
 
 # articles
 fh_no = open(d + '/article-lengths.txt', 'w')
 for ext in 'azw', 'azw3':
-        for f in glob.glob(os.environ['HOME'] + '/.kindle/documents/*.' + ext):
-                c = get_collection(f)
-                fh = fhs.get(c, fh_no)
-                print_entry(file_infos(f, 'mobi'), fh)
+    for f in glob.glob(os.environ['HOME'] + '/.kindle/documents/*.' + ext):
+        c = get_collection(f)
+        fh = fhs.get(c, fh_no)
+        print_entry(file_infos(f, 'mobi'), fh)
 fh_no.close()
 
 for fh in fhs.values():
-        fh.close()
+    fh.close()
 
 # reset the colours, because ffs calibre.
 sys.stderr.write('\033[0m')
