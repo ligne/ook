ecb0e5c1a461c13c348f2bb1cfd9251bb5400f1c
Matthew Boyle <matt.boyle@gmail.com>
2016-03-01 20:21:25 +0000

    tweak suggestion script to use more than one file.

--- suggestions.py
+++ suggestions.py
@@ -13,17 +13,22 @@ def show_nearby(df, index, size):
 
 # read in the options.
 parser = argparse.ArgumentParser()
-parser.add_argument('csv_file')
-parser.add_argument('size', type=int, nargs='?', default=10)
+parser.add_argument('args', nargs='+')
 args = parser.parse_args()
 
-f = args.csv_file
-size = args.size
+files = args.args
 
-# read in the CSV, sort it, and set the index to match the new order.
-df = pd.read_csv(f, sep='\t', names=['words', 'filename', 'title'])  \
-       .sort(['words'])                                              \
+try:
+    size = int(files[-1])
+    files.pop()
+except:
+    size = 10
+
+df = pd.concat([pd.read_csv(f, sep='\t', names=['words', 'filename', 'title']) for f in files])  \
+       .sort(['words'])         \
        .reset_index(drop=True)
+
+# read in the CSVs, sort them, and set the index to match the new order.
 
 median_ix = int(math.floor(len(df.index)/2))
 mean_ix = df[df.words > df.mean().words].index[0]
@@ -38,3 +43,4 @@ suggestion_median = int(math.floor(len(suggestions.index)/2))
 for row in show_nearby(suggestions, suggestion_median, size).iterrows():
     print '{words:7d}  {title}'.format(**row[1])
 
+# vim: ts=4 : sw=4 : et
