fe8b25b7ea3293f3ec5f256ae26a01e60519aa36
Matthew Boyle <matt.boyle@gmail.com>
2016-03-04 13:20:18 +0000

    track clean.py too.

---
+++ clean.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import sys, os, glob, pipes
+
+kindle_dir = '/run/media/mlb/Kindle/documents/'
+
+dirs = [
+    '',
+    'articles/',
+    'books/',
+    'non-fiction/',
+    'short-stories/',
+]
+
+def clean_files(d):
+    for item in glob.glob(d + '*.sdr/'):
+        head, sep, tail = item.partition('.sdr/')
+
+        head = head.replace('[', '[[]')
+
+        cruft = True
+
+        for f in glob.glob(head + '.*'):
+            if f.endswith('.sdr'):
+                continue
+            else:
+                cruft = False
+
+        if cruft:
+            print 'rm -r {}'.format(pipes.quote(item))
+
+for d in dirs:
+    clean_files(kindle_dir + d)
+
+
+# vim: ts=4 : sw=4 : et
