7c40c0a8ac63ba258b61ecf5d774411b45277b36
Matthew Boyle <matt.boyle@gmail.com>
2016-06-08 20:40:25 +0100

    don't sync the articles directory, and leave behind the excludes files.

--- copy.sh
+++ copy.sh
@@ -6,13 +6,9 @@ kindle_dir=/run/media/mlb/Kindle/documents/
   exit 1
 }
 
-for dir in articles books short-stories non-fiction
+for dir in books short-stories non-fiction
 do
   rsync -ha "$dir/" "$kindle_dir/$dir" "$@"                      \
-        --remove-source-files --info=COPY,DEL,MISC,NAME,SYMSAFE  \
-        --exclude-from excludes/articles                         \
-        --exclude-from excludes/books                            \
-        --exclude-from excludes/short-stories                    \
-        --exclude-from excludes/non-fiction
+        --remove-source-files --info=COPY,DEL,MISC,NAME,SYMSAFE
 done
 
