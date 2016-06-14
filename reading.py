c13ed4dbaea27a7726614df00acf34280e63e0fa
Matthew Boyle <matt.boyle@gmail.com>
2016-06-14 12:35:04 +0100

    loads of miscellaneous changes.
    + new graphs of reading rate, new authors per year, histogram of average
    ratings.
    + factor out the changed pages pipeline into its own function.
    + improve commenting.
    + convert the Date {Added,Read} columns to timestamps once and for all.
    + only write the ebook wordcount CSV out when it has changed.

--- reading.py
+++ reading.py
@@ -41,45 +41,43 @@ for (ii, val) in page_counts:
     df.loc[df['Book Id'] == ii,'Number of Pages'] = val
 
 
+df = df.set_index(pd.to_datetime(df['Date Added']))
+df['Date Read'] = pd.to_datetime(df['Date Read'])
+
+
 ################################################################################
 
-def added_pages(shelf):
-    pending = df[df['Exclusive Shelf'] == shelf]
-    if shelf == 'read':
-        pending = pending.dropna(subset=['Date Read'])
-
-    return pending.set_index(pd.to_datetime(pending['Date Added']))  \
+# from shelf, in direction = date added/read.
+def changed_pages(df, shelf, direction):
+    books = df[df['Exclusive Shelf'] == shelf]
+    return books.set_index(pd.to_datetime(books[direction]))  \
+                  ['Number of Pages']  \
                   .resample('D', how='sum')  \
                   .reindex(index=ix)  \
-                  .fillna(0) \
-                  .cumsum()['Number of Pages']
+                  .fillna(0)
+
+
+def added_pages(shelf):
+    pending = df
+    # ignore read books where no end date is set
+    if shelf == 'read':
+        pending = df.dropna(subset=['Date Read'])
+    return changed_pages(pending, shelf, 'Date Added').cumsum()
 
 
 def completed_pages(shelf):
-    pending = df[df['Exclusive Shelf'] == shelf]
-    pending = pending[pending['Date Read'] > '2016']
-
-    return pending.set_index(pd.to_datetime(pending['Date Read']))  \
-                  .resample('D', how='sum')  \
-                  .reindex(index=ix)  \
-                  ['Number of Pages'] \
-                  .fillna(0) \
-                  .cumsum()
+    return changed_pages(df, shelf, 'Date Read').cumsum()
 
 
-def added_ebook_pages():
+def ebook_pages():
     # get current value from actual files
     current_words = get_ebook_words()
 
-    # read from local data cache
+    # read local data cache, and update if necessary
     df = pd.read_csv(EBOOK_WORDCOUNTS, index_col=0, header=None, parse_dates=True, squeeze=True)
-
-    # update if it's changed
     if df.ix[-1] != current_words:
         df.ix[pd.to_datetime('today')] = current_words
-
-    # save new data
-    df.to_csv(EBOOK_WORDCOUNTS)
+        df.to_csv(EBOOK_WORDCOUNTS)
 
     # fill out the missing values
     s = df.reindex(index=ix).fillna(method='ffill')
@@ -107,13 +105,7 @@ def get_ebook_words():
 
 # in pages per day...
 def daily_reading_rate():
-    completed = df.dropna(subset=['Date Read'])
-    completed = completed.set_index(pd.to_datetime(completed['Date Read']))  \
-                         .resample('D', how='sum')  \
-                         .reindex(index=ix)  \
-                         .fillna(0)
-
-    return pd.expanding_mean(completed['Number of Pages'])
+    return pd.expanding_mean(changed_pages(df, 'read', 'Date Read'))
 
 
 # ...or pages per year
@@ -139,12 +131,12 @@ def save_image(df, name):
     plt.close()
 
 
-#################################################################################
-
-if __name__ == "__main__":
+# draw graphs of my backlog over time, both as a number of pages and scaled by
+# reading rate.
+def backlog():
     p = pd.DataFrame({
         'elsewhere': added_pages('elsewhere'),
-        'ebooks'   : added_ebook_pages(),
+        'ebooks'   : ebook_pages(),
         'pending'  : added_pages('currently-reading') + added_pages('pending') + added_pages('read') - completed_pages('read'),
     }, index=ix, columns=['pending', 'ebooks', 'elsewhere'])
 
@@ -156,8 +148,66 @@ if __name__ == "__main__":
     save_image(p, 'backlog')
 
 
-    ### histogram of ratings ###################################################
-    ax = df['Average Rating'].plot(kind='hist', bins=100)
+# plot average scores as a histogram
+def draw_rating_histogram(df):
+    ax = df['Average Rating'].plot(kind='hist', bins=100, title='Average Ratings')
     ax.set_xlim(1,5)
     plt.savefig('images/average_scores.png')
+    plt.close()
+
+
+# number of new authors a year
+def new_authors(df):
+    authors = df.dropna(subset=['Date Read']).sort('Date Read')
+
+    authors = authors.groupby('Author')
+
+    # how many new authors a year
+    first = authors.first()
+    first['year'] = first['Date Read'].dt.year
+    print first['year']
+    first.groupby('year').size().plot()
+
+    # force the bottom of the graph to zero
+    ylim = plt.ylim()
+    plt.ylim([ min(ylim[0], 0), ylim[1] ])
+
+    # prettify and save
+    name = 'new_authors'
+    plt.savefig('images/{}.png'.format(name), bbox_inches='tight')
+    plt.close()
+
+
+# plot reading rate so far.
+def reading_rate():
+    pending = df.dropna(subset=['Date Read'])
+    completed = changed_pages(pending, 'read', 'Date Read')
+
+    current_pages = df[df['Exclusive Shelf'] == 'currently-reading']['Number of Pages'].sum()
+
+    reading = completed.copy()
+    reading.ix[tomorrow] = current_pages
+
+    p = pd.DataFrame({
+        'Completed': pd.expanding_mean(completed),
+        'Reading':   pd.expanding_mean(reading).ix[-2:],
+    }, index=reading.index)
+
+    p.plot(title='Pages read per day')
+    name = 'rate'
+
+    # prettify and save
+    plt.grid(True)
+    #plt.savefig('images/{}.png'.format(name), bbox_inches='tight')
+    plt.savefig('images/{}.png'.format(name))
+    plt.close()
+
+
+#################################################################################
+
+if __name__ == "__main__":
+    backlog()
+    new_authors(df)
+    draw_rating_histogram(df)
+    reading_rate()
 
