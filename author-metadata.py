e03f9bdaa8cbb6168be1725818080ac865f2c299
Matthew Boyle <matt.boyle@gmail.com>
2016-12-08 14:10:23 +0000

    ignore the to-read shelf.

--- author-metadata.py
+++ author-metadata.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 
+import time
+
 import reading
 from reading.author import Author
 
 df = reading.get_books()
 
-all_authors = list(set(df.Author.values))
+df = reading.on_shelves(df, ['read', 'currently-reading', 'pending', 'elsewhere', 'ebooks'])
 
-for authors in zip(*[iter(all_authors)]*1):
+all_authors = sorted(list(set(df.Author.values)))
+
+for authors in zip(*[iter(all_authors)]*10):
     for author in authors:
         Author(author).fetch_missing()
-        print
-    print
-    break
 
+
+Author.save()
 
