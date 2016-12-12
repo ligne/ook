e47d3f3972ef54d525b1e89f94f794673b4a00a5
Matthew Boyle <matt.boyle@gmail.com>
2016-12-12 13:41:15 +0000

    rejig things so that it can be invoked from elsewhere.

--- gr-update.py
+++ gr-update.py
@@ -8,71 +8,76 @@ import pandas as pd
 
 import reading
 
-
-def get_books(*args):
-    df = reading.get_books(no_fixes=True, *args)
-
-    columns = [
-        'Title',
-        'Author',
-        'Date Added',
-        'Bookshelves',
-        'Exclusive Shelf',
-        'Series',
-        'Entry',
-    ]
-
-    return df[columns].sort_index()
+columns = [
+    'Title',
+    'Author',
+    'Date Added',
+    'Bookshelves',
+    'Exclusive Shelf',
+    'Series',
+    'Entry',
+    'Original Publication Year',
+]
 
 
-df_old = get_books()
-df_new = get_books(sys.argv[1])
+def compare(df_old, df_new):
+    df_old = df_old[columns]
+    df_new = df_new[columns]
 
-# force both to use the same index
-ix = df_old.index | df_new.index
-df_old = df_old.reindex(ix).fillna('')
-df_new = df_new.reindex(ix).fillna('')
 
-ne_stacked = (df_old != df_new).stack()
-changed = ne_stacked[ne_stacked]
+    # force both to use the same index
+    ix = df_old.index | df_new.index
+    df_old = df_old.reindex(ix).fillna('')
+    df_new = df_new.reindex(ix).fillna('')
 
-for (index, changes) in changed.groupby(level=0):
-    old_row = df_old.ix[index]
-    new_row = df_new.ix[index]
+    ne_stacked = (df_old != df_new).stack()
+    changed = ne_stacked[ne_stacked]
 
-    if not new_row['Author']:
-        print "Removed '{Title}' by {Author}".format(**old_row)
-        print '  Bookshelves: {Bookshelves}'.format(**old_row)
-    elif not old_row['Author']:
-        fmt = "Added '{Title}' by {Author}"
-        if new_row['Series']:
-            fmt += ' ({Series}, book {Entry})'
-        print fmt.format(**new_row)
-        # also show any bookshelves it's been added to
-        print '  Bookshelves: {Bookshelves}'.format(**new_row)
-    else:
-        print '{Author}, {Title}'.format(**old_row)
+    for (index, changes) in changed.groupby(level=0):
+        old_row = df_old.ix[index]
+        new_row = df_new.ix[index]
 
-        for col in changes.index.get_level_values(1).values:
-            if col == 'Bookshelves':
-                old = set(old_row[col].split(', '))
-                new = set(new_row[col].split(', '))
+        if not new_row['Author']:
+            print "Removed '{Title}' by {Author}".format(**old_row)
+            print '  Bookshelves: {Bookshelves}'.format(**old_row)
+        elif not old_row['Author']:
+            fmt = "Added '{Title}' by {Author}"
+            if new_row['Series']:
+                fmt += ' ({Series}, book {Entry})'
+            print fmt.format(**new_row)
+            # also show any bookshelves it's been added to
+            print '  Bookshelves: {Bookshelves}'.format(**new_row)
+        else:
+            print '{Author}, {Title}'.format(**old_row)
 
-                added   = new - old - set([new_row['Exclusive Shelf']])
-                removed = old - new - set([old_row['Exclusive Shelf']])
+            for col in changes.index.get_level_values(1).values:
+                if col == 'Bookshelves':
+                    old = set(old_row[col].split(', '))
+                    new = set(new_row[col].split(', '))
 
-                if not (added or removed):
-                    continue
+                    added   = new - old - set([new_row['Exclusive Shelf']])
+                    removed = old - new - set([old_row['Exclusive Shelf']])
 
-                print '{}:'.format(col)
-                if removed:
-                    print '\t-{}'.format(', -'.join(removed)),
-                if added:
-                    print '\t+{}'.format(', +'.join(added)),
-                print
-            else:
-                print '{}:\n\t{} -> {}'.format(col, old_row[col], new_row[col])
+                    if not (added or removed):
+                        continue
 
-    print '----'
+                    print '{}:'.format(col)
+                    if removed:
+                        print '\t-{}'.format(', -'.join(removed)),
+                    if added:
+                        print '\t+{}'.format(', +'.join(added)),
+                    print
+                else:
+                    print '{}:\n\t{} -> {}'.format(col, old_row[col], new_row[col])
+
+        print '----'
+
+
+if __name__ == "__main__":
+    compare(
+        reading.get_books(no_fixes=True),
+        reading.get_books(no_fixes=True, filename=sys.argv[1]),
+    )
+
 
 # vim: ts=4 : sw=4 : et
