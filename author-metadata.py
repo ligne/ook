3d823861c592a56de941cb64c619403268b0f3a1
Matthew Boyle <matt.boyle@gmail.com>
2017-01-04 13:11:26 +0000

    get metadata for ebooks and their authors.

--- author-metadata.py
+++ author-metadata.py
@@ -8,6 +8,19 @@ from reading.author import Author
 from reading.book import Book
 
 
+df = reading.ebooks.get_books(fix_names=False)
+
+for ix, book in df.iterrows():
+    try:
+        b = Book(book['Title'], book['Author'], book['Language'])
+        b.fetch_missing()
+        if len(b.get('AQIDs', [])) == 1:
+            Author(book['Author'], qid=b.get('AQIDs')[0])
+    except Exception as e:
+        print e
+
+################################################################################
+
 df = reading.get_books(fix_names=False)
 
 df = reading.on_shelves(df, [
@@ -30,6 +43,8 @@ for author in authors:
     except Exception as e:
         print e
 
+Book.save()
 Author.save()
+
 
 # vim: ts=4 : sw=4 : et
