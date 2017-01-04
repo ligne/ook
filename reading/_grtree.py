de8a658b9da6b9a4ca45f02097300d61fa8caf52
Matthew Boyle <matt.boyle@gmail.com>
2017-01-04 20:10:46 +0000

    class for dealing with the GR API.

---
+++ reading/_grtree.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import time
+import yaml
+from xml.etree import ElementTree
+
+
+# basic operations on a GR xml entity
+class GRTree():
+    # load the config to get the GR API key.
+    with open('data/config.yml') as fh:
+        config = yaml.load(fh)
+
+    def __init__(self, grid):
+        import requests
+
+        r = requests.get('https://www.goodreads.com/book/show/{}.xml'.format(grid), params={
+            'key': self.config['GR Key'],
+        })
+        time.sleep(1)
+        self._tree = ElementTree.fromstring(r.content)
+
+
+    # returns the string at $path
+    def get_text(self, path):
+        return self._tree.find(path).text.encode('utf-8')
+
+
+    # returns all the nodes at $path
+    def get_values(self, path):
+        return self._tree.findall(path)
+
+
+# vim: ts=4 : sw=4 : et
