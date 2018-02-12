c05adf912295c000bd209528d27c9ace34c63c8b
Matthew Boyle <matt.boyle@gmail.com>
2018-02-12 18:29:23 +0000

    list ebooks and to-read too.

--- owned.py
+++ owned.py
@@ -10,16 +10,51 @@ import reading
 
 # FIXME also books that i want to buy. and books that i have ebooks of?
 
-df = reading.get_books()
-
-owned_shelves = [
+df = reading.get_books(shelves=[
     'pending',
     'elsewhere',
-]
+])
 
+# deduplicate multiple volumes
 df = df.drop_duplicates(['Author', 'Title'])
 
-g = reading.on_shelves(df, owned_shelves).sort_values(['Author', 'Title']).groupby('Author')
+g = df.sort_values(['Author', 'Title']).groupby('Author')
+
+for author in sorted(g.groups.keys()):
+    print '{}'.format(author)
+    for ix, row in g.get_group(author).iterrows():
+        print '* {Title}'.format(**row)
+    print
+
+print '----'
+print 'Ebooks'
+
+df = reading.get_books(shelves=[
+    'kindle',
+])
+
+# deduplicate multiple volumes
+df = df.drop_duplicates(['Author', 'Title'])
+
+g = df.sort_values(['Author', 'Title']).groupby('Author')
+
+for author in sorted(g.groups.keys()):
+    print '{}'.format(author)
+    for ix, row in g.get_group(author).iterrows():
+        print '* {Title}'.format(**row)
+    print
+
+print '----'
+print 'To read'
+
+df = reading.get_books(shelves=[
+    'to-read',
+])
+
+# deduplicate multiple volumes
+df = df.drop_duplicates(['Author', 'Title'])
+
+g = df.sort_values(['Author', 'Title']).groupby('Author')
 
 for author in sorted(g.groups.keys()):
     print '{}'.format(author)
