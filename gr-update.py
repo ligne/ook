1ee445eb2178cc902d46e076fd4bca119825d64c
Matthew Boyle <matt.boyle@gmail.com>
2018-05-23 19:16:29 +0100

    put the new output ahead of the legacy.

--- gr-update.py
+++ gr-update.py
@@ -21,11 +21,11 @@ old = pd.read_csv('gr-api.csv', index_col=0, dtype=object)
 
 #df.sort_index().to_csv('gr-api.csv', float_format='%.f')
 
+reading.compare._changed(old.fillna(''), df.fillna(''))
+
 diff = compare(old, df)
 if diff:
-    print(diff)
     print('******************')
-
-reading.compare._changed(old.fillna(''), df.fillna(''))
+    print(diff)
 
 # vim: ts=4 : sw=4 : et
