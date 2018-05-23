5f64ddb00817cb7b2766d61fb32822e2f2333eac
Matthew Boyle <matt.boyle@gmail.com>
2018-05-23 19:00:43 +0100

    new version of the goodreads updater.
    this one uses the comparison module instead.

--- gr-update.py
+++ gr-update.py
@@ -1,89 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
+import time
+import yaml
 import datetime
-
+import re
+import sys
+from xml.etree import ElementTree
+from dateutil.parser import parse
 import pandas as pd
 
-import reading
-
-columns = [
-    'Title',
-    'Author',
-    'Date Added',
-    'Bookshelves',
-    'Exclusive Shelf',
-    'Series',
-    'Entry',
-    'Original Publication Year',
-]
+import reading.goodreads
+from reading.compare import compare
 
 
-def compare(df_old, df_new):
-    df_old = df_old[columns]
-    df_new = df_new[columns]
+df = reading.goodreads.get_books()
+df = df.set_index('Book Id')
 
-    # force both to use the same index
-    ix = df_old.index | df_new.index
-    df_old = df_old.reindex(ix).fillna('')
-    df_new = df_new.reindex(ix).fillna('')
+#patches = pd.read_csv('data/goodreads_library_export.csv', index_col=0)
+#patches.loc[:,'Number of Pages'].fillna('', inplace=True)
 
-    ne_stacked = (df_old != df_new).stack()
-    changed = ne_stacked[ne_stacked]
+#df['Number of Pages'] = df['Number of Pages'].combine(patches['Number of Pages'], lambda x2, x1: x1 if x1 else x2)
+#df.loc[:,'Number of Pages'].fillna('', inplace=True)
+#
+#df['Number of Pages'] = df['Number of Pages'].astype(str)
 
-    for (index, changes) in changed.groupby(level=0):
-        old_row = df_old.ix[index]
-        new_row = df_new.ix[index]
+#df['Original Publication Year'] = patches['Original Publication Year']
 
-        if not new_row['Author']:
-            print "Removed '{Title}' by {Author}".format(**old_row)
-            print '  Bookshelves: {Bookshelves}'.format(**old_row)
-        elif not old_row['Author']:
-            fmt = "Added '{Title}' by {Author}"
-            if new_row['Series']:
-                fmt += ' ({Series}, book {Entry})'
-            print fmt.format(**new_row)
-            # also show any bookshelves it's been added to
-            print '  Bookshelves: {Bookshelves}'.format(**new_row)
-        else:
-            if (changes.index.get_level_values(1).values == ['Date Added']).all():
-                continue
+old = pd.read_csv('gr-api.csv', index_col=0, dtype=object)
 
-            print '{Author}, {Title}'.format(**old_row)
+#df.sort_index().to_csv('gr-api.csv', float_format='%.f')
 
-            for col in changes.index.get_level_values(1).values:
-                if col == 'Date Added' and old_row[col] == new_row[col]:
-                    continue
+diff = compare(old, df)
+if diff:
+    print(diff)
+    print('******************')
 
-                if col == 'Bookshelves':
-                    old = set(old_row[col].split(', '))
-                    new = set(new_row[col].split(', '))
-
-                    added   = new - old - set([new_row['Exclusive Shelf']])
-                    removed = old - new - set([old_row['Exclusive Shelf']])
-
-                    if not (added or removed):
-                        continue
-
-                    print '{}:'.format(col)
-                    if removed:
-                        print '  -{}'.format(', -'.join(removed)),
-                    if added:
-                        print '  +{}'.format(', +'.join(added)),
-                    print
-                else:
-                    print '{}:\n  {} -> {}'.format(col, old_row[col], new_row[col])
-
-        print
-        print '----'
-
-
-if __name__ == "__main__":
-    compare(
-        reading.get_gr_books(no_fixes=True, fix_names=False),
-        reading.get_gr_books(no_fixes=True, fix_names=False, filename=sys.argv[1]),
-    )
-
+reading.compare._changed(old.fillna(''), df.fillna(''))
 
 # vim: ts=4 : sw=4 : et
