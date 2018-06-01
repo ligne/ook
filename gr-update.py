bf5716ad412f07514864aef793073d51109e2eff
Matthew Boyle <matt.boyle@gmail.com>
2018-06-01 17:33:46 +0100

    make it simpler to choose whether to save/discard updates.

--- gr-update.py
+++ gr-update.py
@@ -2,17 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import argparse
 
 import reading.goodreads
 from reading.compare import compare
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--ignore-changes', action='store_true')
+args = parser.parse_args()
 
 df = reading.goodreads.get_books()
 df = df.set_index('Book Id')
 
 old = pd.read_csv('gr-api.csv', index_col=0, dtype=object)
 
-#df.sort_index().to_csv('gr-api.csv', float_format='%.f')
+if not args.ignore_changes:
+    df.sort_index().to_csv('gr-api.csv', float_format='%.f')
 
 reading.compare._changed(old.fillna(''), df.fillna(''))
 
