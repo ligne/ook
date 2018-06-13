f42267b4a137564c5448c569127d26a6518434fd
Matthew Boyle <matt.boyle@gmail.com>
2018-06-13 20:54:57 +0100

    class representing a collection of {e,}books.

---
+++ reading/collection.py
@@ -0,0 +1,132 @@
+# vim: ts=4 : sw=4 : et
+
+import pandas as pd
+
+
+GR_CSV    = 'data/goodreads.csv'
+GR_CSV    = 'gr-api.csv'           # FIXME
+EBOOK_CSV = 'data/ebooks.csv'
+EBOOK_CSV = 'data/wordcounts.csv'  # FIXME
+
+words_per_page = 390
+
+
+################################################################################
+
+def _get_gr_books(csv=GR_CSV):
+    # FIXME Published as a date would be nice too, except pandas doesn't
+    # support very old dates.
+    df = pd.read_csv(csv, index_col=0, parse_dates=[
+        'Date Added',
+        'Date Started',
+        'Date Read',
+        'Scheduled'
+    ])
+
+#    df = df.rename(columns={
+#        'Date Added': 'Added',
+#        'Date Started': 'Started',
+#        'Date Read': 'Read',
+#        'Original Publication Year': 'Published',
+#        'Number of Pages': 'Pages',
+#        'Exclusive Shelf': 'Shelf',
+#        'My Rating': 'Rating',
+#        'Average Rating': 'AvgRating',
+#        'Work Id': 'Work',
+#    })
+    df = df.drop(['Bookshelves'], axis='columns')
+
+    return df
+
+
+def _save_gr_books(csv, df):
+    pass
+
+
+################################################################################
+
+def _get_kindle_books(csv=EBOOK_CSV):
+    df = pd.read_csv(csv, sep='\t', index_col=False)  # FIXME use proper csv
+
+    # fix author from metadata?
+    # split title, subtitle, volume
+    # fix up title (and subtitle?)
+
+    # calculate page count
+    df['Pages'] = df.Words / words_per_page
+
+    # set missing columns
+    df = df.assign(
+        Binding='ebook',
+        Borrowed=False,
+        Shelf='kindle',
+    )
+
+    df.Author.fillna('', inplace=True)
+
+    df['Added'] = pd.to_datetime(df.mtime, unit='s')
+
+    df = df.drop(['Words', 'file', 'mtime' ], axis='columns')
+
+    # set an index that won't clash
+    return df.set_index([['_'+str(ii) for ii in df.index]])
+
+
+# FIXME maybe want this to not require pandas?
+def _save_kindle_books(csv):
+    pass
+
+
+################################################################################
+
+class Collection():
+
+    # options:
+    #   control dtype?
+    #   control what fix-ups are enabled (for linting)
+    #   control merging volumes
+    #   control dedup (duplicate books;  duplicate ebooks;  ebooks that are
+    #       also in goodreads)
+    #   control visibility of later books in series
+
+    def __init__(self, df=None,
+        shelves=None, categories=None, languages=None,
+        gr_csv=GR_CSV, ebook_csv=EBOOK_CSV):
+
+        # just wrap it
+        if df:
+            self.df = df.copy()
+            return self
+
+        # otherwise load and concatenate the CSV files
+        df = pd.concat([
+            _get_gr_books(gr_csv),
+            _get_kindle_books(ebook_csv),
+        ])
+
+        # apply filters on shelf, language, category.
+        if categories:
+            df = df[df.Category.isin(categories)]
+        else:
+            # ignore articles unless explicitly requested
+            df = df[~df.Category.isin(['articles'])]
+
+        if languages:
+            df = df[df['Language'].isin(languages)]
+        if shelves:
+            df = df[df['Exclusive Shelf'].isin(shelves)]
+
+        # load information about the authors
+
+        self.df = df
+
+
+    # save to disk.  FIXME must only apply to one file?
+    def save():
+        pass
+
+
+if __name__ == "__main__":
+    print(Collection().df)
+    print(Collection().df.dtypes)
+
---
+++ tests/test_reading_collection.py
@@ -0,0 +1,99 @@
+# vim: ts=4 : sw=4 : et
+
+from nose.tools import *
+
+import pandas as pd
+import numpy as np
+from io import StringIO
+
+import reading.collection
+
+
+def test__get_gr_books():
+    # FIXME use a test csv
+    df = reading.collection._get_gr_books()
+
+    assert_equals(sorted(df.columns), sorted([
+        'Date Added', #'Added',
+        'Author',
+        'Author Id',
+        'Average Rating', #'AvgRating',
+        'Binding',
+        'Borrowed',
+        'Category',
+        'Entry',
+        'Language',
+        'Number of Pages', #'Pages',
+        'Original Publication Year', #'Published',
+        'My Rating', #'Rating',
+        'Date Read', #'Read',
+        'Scheduled',
+        'Series',
+        'Series Id',
+        'Exclusive Shelf', #'Shelf',
+        'Date Started', #'Started',
+        'Title',
+        'Work Id', #'Work',
+    ]))
+
+    eq_(set(df.Category.values), set([
+        'novel',
+        'short-stories',
+        'non-fiction',
+        'graphic',
+        np.nan
+    ]))
+
+#     eq_(list(zip(df.columns, df.dtypes)), [
+#     ])
+
+    b = df.loc[2366570]  # Les Chouans
+
+    # timestamp columns are ok
+    eq_(b['Date Added'].strftime('%F'), '2016-04-18') #eq_(b.Added.strftime('%F'), '2016-04-18')
+    eq_(b['Date Started'].strftime('%F'), '2016-09-08') #eq_(b.Started.strftime('%F'), '2016-09-08')
+    eq_(b['Date Read'].strftime('%F'), '2016-11-06') #eq_(b.Read.strftime('%F'), '2016-11-06')
+    eq_(b['Original Publication Year'], 1829.) #eq_(b.Published, 1829.)  # FIXME pandas can't do very old dates...
+
+    b = df.loc[8861500]  # The Dain Curse
+    # scheduled is a datetime
+    eq_(b.Scheduled.year, 2018)
+    eq_(b.Scheduled.strftime('%F'), '2018-01-01')
+
+    b = df.loc[28595808]  # The McCabe Reader
+    # missing publication year
+    ok_(np.isnan(b['Original Publication Year'])) #ok_(np.isnan(b.Published))
+
+
+def test__get_kindle_books():
+    # FIXME use a test csv
+    df = reading.collection._get_kindle_books()
+
+    eq_(set(df.columns), set([
+        'Author',
+        'Binding',
+        'Borrowed',
+        'Category',
+        'Added',
+        'Shelf',
+        'Language',
+        'Pages',
+        'Title',
+    ]))
+
+    eq_(df.Binding.unique(), ['ebook'])
+    eq_(df.Borrowed.unique(), [False])
+    eq_(df.Shelf.unique(), ['kindle'])
+
+#     eq_(list(zip(df.columns, df.dtypes)), [
+#     ])
+
+    eq_(df[df.Author == 'Edith Birkhead'].Added.dt.strftime('%F').values[0], '2013-02-06', 'Added is the correct datetime')
+
+    # author field is never null
+    # FIXME do we actually care?
+    eq_(len(df[df.Author.isnull()]), 0)
+
+    # set an index that won't clash with goodreads's.
+    eq_(list(df.index[0:3]), ['_0', '_1', '_2'])
+
