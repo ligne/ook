87e9c78d1dbf43f1edb026fd67a21539b33ecf18
Matthew Boyle <matt.boyle@gmail.com>
2018-06-15 13:03:09 +0100

    use a Collection for updating.

--- gr-update.py
+++ gr-update.py
@@ -6,6 +6,8 @@ import argparse
 
 import reading.goodreads
 from reading.compare import compare
+from reading.collection import Collection
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--ignore-changes', action='store_true')
@@ -13,14 +15,23 @@ args = parser.parse_args()
 
 df = reading.goodreads.get_books()
 
-old = reading.collection._get_gr_books()
+# FIXME shortcut for this, please!
+old = Collection(shelves=[
+    'read',
+    'currently-reading',
+    'pending',
+    'elsewhere',
+    'library',
+    'ebooks',
+    'to-read',
+])
 
 if not args.ignore_changes:
     df.sort_index().to_csv('gr-api.csv', float_format='%g')
 
-reading.compare._changed(old.fillna(''), df.fillna(''))
+reading.compare._changed(old.df.fillna(''), df.fillna(''))
 
-diff = compare(old, df)
+diff = compare(old.df, df)
 if diff:
     print('******************')
     print(diff)
