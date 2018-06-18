82403fdf3b6afe1984c28dec29b0e19d1e6b0c91
Matthew Boyle <matt.boyle@gmail.com>
2018-06-18 18:42:41 +0100

    admire my complete lack of tests.

---
+++ tests/test_reading_compare.py
@@ -0,0 +1,39 @@
+# vim: ts=4 : sw=4 : et
+
+import nose
+import pandas as pd
+
+import reading.compare
+
+
+def test_compare():
+    pass
+
+
+def test_added_book():
+    pass
+
+
+def test_removed_book():
+    pass
+
+
+def test_changed_book():
+    pass
+
+
+def test_started_book():
+    # python3 reading/compare.py old/csvs/gr-1520252718.csv old/csvs/gr-1520255009.csv
+    pass
+
+
+def test_finished_book():
+    # python3 reading/compare.py old/csvs/gr-1520013450.csv old/csvs/gr-1520252718.csv
+    pass
+
+
+# python3 reading/compare.py old/csvs/gr-1521002014.csv gr-api.csv
+# python3 reading/compare.py old/csvs/gr-1520483615.csv gr-api.csv
+# python3 reading/compare.py old/csvs/gr-1520341516.csv gr-api.csv
+# python3 reading/compare.py old/csvs/gr-1522985618.csv gr-api.csv  # nuit du carrefour
+
