52f598be7c79e91cda8e844a690724515c51ab40
Matthew Boyle <matt.boyle@gmail.com>
2018-06-19 18:27:51 +0100

    add a couple of tests for the compare module.
    still needs something a bit more serious for the compare() method itself.

--- tests/test_reading_compare.py
+++ tests/test_reading_compare.py
@@ -1,7 +1,9 @@
 # vim: ts=4 : sw=4 : et
 
-import nose
-import pandas as pd
+from nose.tools import *
+from io import StringIO
+
+from reading.collection import Collection
 
 import reading.compare
 
@@ -10,30 +12,54 @@ def test_compare():
     pass
 
 
-def test_added_book():
+def test__added():
+    c = Collection(gr_csv=StringIO("""
+Book Id,Added,Author,Author Id,AvgRating,Binding,Borrowed,Category,Entry,Language,Pages,Published,Rating,Read,Scheduled,Series,Series Id,Shelf,Started,Title,Work
+26570162,2017-07-27,Matthew Lewis,7798465,3.8,Paperback,False,novels,,en,339,1796,0,,,,,pending,,The Monk,3095060
+"""))
+
+    eq_(reading.compare._added(c.df.iloc[0]), """
+Added 'The Monk' by Matthew Lewis
+  novels
+""".lstrip())
+
+
+def test__removed():
+    c = Collection(gr_csv=StringIO("""
+Book Id,Added,Author,Author Id,AvgRating,Binding,Borrowed,Category,Entry,Language,Pages,Published,Rating,Read,Scheduled,Series,Series Id,Shelf,Started,Title,Work
+26570162,2017-07-27,Matthew Lewis,7798465,3.8,Paperback,False,novels,,en,339,1796,0,,,,,pending,,The Monk,3095060
+"""))
+
+    eq_(reading.compare._removed(c.df.iloc[0]), """
+Removed 'The Monk' by Matthew Lewis
+""".lstrip())
+
+
+def test__changed():
     pass
 
 
-def test_removed_book():
-    pass
+def test__started():
+    c = Collection(gr_csv=StringIO("""
+Book Id,Added,Author,Author Id,AvgRating,Binding,Borrowed,Category,Entry,Language,Pages,Published,Rating,Read,Scheduled,Series,Series Id,Shelf,Started,Title,Work
+8899970,2018-02-24,Graham Greene,2533,3.66,Paperback,False,novels,,en,190,1936,4,,,,,currently-reading,2018-03-05,A Gun for Sale,151810
+"""))
+
+    eq_(reading.compare._started(c.df.iloc[0]), """
+Started 'A Gun for Sale' by Graham Greene
+""".lstrip())
 
 
-def test_changed_book():
-    pass
+def test__finished():
+    c = Collection(gr_csv=StringIO("""
+Book Id,Added,Author,Author Id,AvgRating,Binding,Borrowed,Category,Entry,Language,Pages,Published,Rating,Read,Scheduled,Series,Series Id,Shelf,Started,Title,Work
+491030,2016-04-18,Iain Banks,7628,3.84,Paperback,False,novels,,en,288,1986,4,2016-08-10,,,,read,2016-07-19,The Bridge,1494168
+"""))
 
-
-def test_started_book():
-    # python3 reading/compare.py old/csvs/gr-1520252718.csv old/csvs/gr-1520255009.csv
-    pass
-
-
-def test_finished_book():
-    # python3 reading/compare.py old/csvs/gr-1520013450.csv old/csvs/gr-1520252718.csv
-    pass
-
-
-# python3 reading/compare.py old/csvs/gr-1521002014.csv gr-api.csv
-# python3 reading/compare.py old/csvs/gr-1520483615.csv gr-api.csv
-# python3 reading/compare.py old/csvs/gr-1520341516.csv gr-api.csv
-# python3 reading/compare.py old/csvs/gr-1522985618.csv gr-api.csv  # nuit du carrefour
+    eq_(reading.compare._finished(c.df.iloc[0]), """
+Finished 'The Bridge' by Iain Banks
+  2016-07-19 00:00:00 → 2016-08-10 00:00:00 (22 days)
+  288 pages, 13 pages/day
+  Rating: 4.0
+""".lstrip())
 
