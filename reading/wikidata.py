9e4bb24422e6649d734e025522bf412105dfdb1a
Matthew Boyle <matt.boyle@gmail.com>
2018-06-25 13:39:39 +0100

    code to run a search against (English) wikipedia.
    it uses a full-text search so it's much better at dealing with mangled terms.

--- reading/wikidata.py
+++ reading/wikidata.py
@@ -20,6 +20,21 @@ def search(term):
     } for res in r.json()['search']]
 
 
+# use wikipedia full-text search.
+def search_harder(term):
+    r = requests.get('https://en.wikipedia.org/w/api.php', params={
+        'action': 'query',
+        'list': 'search',
+        'prop': 'pageprops',
+        'format': 'json',
+        'srsearch': term,
+    })
+    return r.content.decode('utf-8')
+    return [{
+
+    } for res in r.json()['query']['search']]
+
+
 ################################################################################
 
 # fetches an entity by its QID
