74af738ceb83531cc1f8a47e8d9753a09a28f9ed
Matthew Boyle <matt.boyle@gmail.com>
2018-06-26 14:15:47 +0100

    fix up (some of) the older tests.

--- tests/test_reading_metadata.py
+++ tests/test_reading_metadata.py
@@ -8,9 +8,9 @@ import reading.wikidata
 
 
 def test__list_choices():
-#    # nothing
-#    assert_multi_line_equal(reading.metadata._list_choices([]), '')
-#
+    # nothing
+    assert_multi_line_equal(reading.metadata._list_choices([], set(), set()), '')
+
 #    # a couple of authors
 #    assert_multi_line_equal(reading.metadata._list_choices([
 #        #reading.wikidata.search('Iain Banks'),
@@ -23,43 +23,48 @@ def test__list_choices():
 # 2. Iain Banks bibliography
 #      https://www.wikidata.org/wiki/Q16386218
 #'''.lstrip())
-#
+
     assert_multi_line_equal.__self__.maxDiff = None
-#
-#    # books from goodreads title search
-#    assert_multi_line_equal(reading.metadata._list_choices([
-#        {'Ratings': '31583', 'Published': '1853', 'BookId': '182381', 'Work': '1016559', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Cranford'},
-#        {'Ratings': '1515', 'Published': '1859', 'BookId': '2141817', 'Work': '21949576', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'The Cranford Chronicles'},
-#        {'Ratings': '74', 'Published': '2009', 'BookId': '7329542', 'Work': '8965360', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Return to Cranford: Cranford and other stories'},
-#        {'Ratings': '10', 'Published': '2000', 'BookId': '732416', 'Work': '718606', 'Author': 'J.Y.K. Kerr', 'Author Id': '1215308', 'Title': 'Cranford'},
-#        {'Ratings': '428', 'Published': '1864', 'BookId': '222401', 'Work': '215385', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Cranford/Cousin Phillis'},
-#    ]), '''
-#\033[1m 1.\033[0m Cranford
-#      Elizabeth Gaskell
-#      Published: 1853
-#      Ratings: 31583
-#      https://www.goodreads.com/book/show/182381
-# 2. The Cranford Chronicles
-#      Elizabeth Gaskell
-#      Published: 1859
-#      Ratings: 1515
-#      https://www.goodreads.com/book/show/2141817
-# 3. Return to Cranford: Cranford and other stories
-#      Elizabeth Gaskell
-#      Published: 2009
-#      Ratings: 74
-#      https://www.goodreads.com/book/show/7329542
-# 4. Cranford
-#      J.Y.K. Kerr
-#      Published: 2000
-#      Ratings: 10
-#      https://www.goodreads.com/book/show/732416
-# 5. Cranford/Cousin Phillis
-#      Elizabeth Gaskell
-#      Published: 1864
-#      Ratings: 428
-#      https://www.goodreads.com/book/show/222401
-#'''.lstrip())
+
+    # books from goodreads title search
+    assert_multi_line_equal(reading.metadata._list_choices([
+        {'Ratings': '31583', 'Published': '1853', 'BookId': '182381', 'Work': '1016559', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Cranford'},
+        {'Ratings': '1515', 'Published': '1859', 'BookId': '2141817', 'Work': '21949576', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'The Cranford Chronicles'},
+        {'Ratings': '74', 'Published': '2009', 'BookId': '7329542', 'Work': '8965360', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Return to Cranford: Cranford and other stories'},
+        {'Ratings': '10', 'Published': '2000', 'BookId': '732416', 'Work': '718606', 'Author': 'J.Y.K. Kerr', 'AuthorId': '1215308', 'Title': 'Cranford'},
+        {'Ratings': '428', 'Published': '1864', 'BookId': '222401', 'Work': '215385', 'Author': 'Elizabeth Gaskell', 'AuthorId': '1413437', 'Title': 'Cranford/Cousin Phillis'},
+    ], set(), set()), '''
+\033[1m 1.\033[0m Cranford\033[0m
+      Elizabeth Gaskell\033[0m
+      Published: 1853
+      Ratings: 31583
+      https://www.goodreads.com/book/show/182381
+      https://www.goodreads.com/author/show/1413437
+ 2. The Cranford Chronicles\033[0m
+      Elizabeth Gaskell\033[0m
+      Published: 1859
+      Ratings: 1515
+      https://www.goodreads.com/book/show/2141817
+      https://www.goodreads.com/author/show/1413437
+ 3. Return to Cranford: Cranford and other stories\033[0m
+      Elizabeth Gaskell\033[0m
+      Published: 2009
+      Ratings: 74
+      https://www.goodreads.com/book/show/7329542
+      https://www.goodreads.com/author/show/1413437
+ 4. Cranford\033[0m
+      J.Y.K. Kerr\033[0m
+      Published: 2000
+      Ratings: 10
+      https://www.goodreads.com/book/show/732416
+      https://www.goodreads.com/author/show/1215308
+ 5. Cranford/Cousin Phillis\033[0m
+      Elizabeth Gaskell\033[0m
+      Published: 1864
+      Ratings: 428
+      https://www.goodreads.com/book/show/222401
+      https://www.goodreads.com/author/show/1413437
+'''.lstrip())
 
     # it's both an author and a work that i have already.
     results = [
