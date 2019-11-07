220c52c141e6797dc837f4f47f9f491b91c29375
Matthew Boyle <matt.boyle@gmail.com>
2019-11-07 19:15:43 +0000

    module to wrap config access in a slightly nicer way.

---
+++ reading/config.py
@@ -0,0 +1,25 @@
+# vim: ts=4 : sw=4 : et
+
+import yaml
+
+
+# value = config('key.name')
+def config(key):
+    with open('data/config.yml') as fh:
+        config = yaml.load(fh)
+
+    for key in key.split('.'):
+        print(key)
+        try:
+            config = config[key]
+        except KeyError:
+            # use defaults and/or emit warning
+            config = None
+
+    return config
+
+
+if __name__ == "__main__":
+    import sys
+    print(config(sys.argv[1]))
+
