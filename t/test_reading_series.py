ed6eea26b25e2c202b9be9cceea63766ac33af56
Matthew Boyle <matt.boyle@gmail.com>
2019-12-04 18:45:49 +0000

    add some tests and start to make things work with pytest.

--- t/test_reading_series.py
+++ t/test_reading_series.py
@@ -1,10 +1,14 @@
 # vim: ts=4 : sw=4 : et
 
-from nose.tools import *
-from xml.etree import ElementTree
+import pytest
+
+from nose.tools import eq_, ok_
+
+from reading.collection import Collection
 
 import reading.series
 from reading.series import _parse_entries, _get_entry, interesting
+from reading.series import Series
 
 
 def test__get_entry():
@@ -77,4 +81,56 @@ def test_interesting():
 def test_ignore():
     eq_(reading.series.ignore(1), False)
     eq_(reading.series.ignore(55486), True)
+
+
+################################################################################
+
+def _get_collection():
+    return Collection(gr_csv='t/data/goodreads-2019-12-04.csv')
+
+
+def test_series():
+    # needs at least *something* to go on
+    with pytest.raises(ValueError):
+        Series()
+
+    # by author name
+    s = Series(author='Beauvoir')
+    assert s, 'Created a series from an author'
+    assert s.label == 'Beauvoir'
+    assert s.order == 'published', 'Authors are read in published order by default'
+    assert s.missing == 'ignore', 'Authors have no missing books to ignore'
+
+    # warning when there are duplicates
+    with pytest.warns(UserWarning):
+        Series(author='Iain Banks')
+
+    # by series name
+    s = Series(series='Culture')
+    assert s, 'Created a series from a name'
+    assert s.label == 'Culture'
+    assert s.order == 'series', 'Series are read in order'
+    assert s.missing == 'ignore'
+
+    # by SeriesID
+    s = Series(series_id=49118)
+    assert s, 'Created a series from an ID'
+    assert s.label == 'Culture'
+    assert s.order == 'series', 'Series are read in order'
+    assert s.missing == 'ignore'
+
+    # FIXME check these last two have the same results
+
+    # series can have multiple authors
+    s = Series(series='Spirou')
+    assert len(set(s.df.Author)) > 1
+
+    # settings
+    s = Series(series='Culture', settings={
+        'order': 'published',
+    })
+    assert s.order == 'published', 'Can override the order of series'
+    assert s.missing == 'ignore', 'By default ignore missing books from series'
+
+    # FIXME also check .missing behaviour
 
