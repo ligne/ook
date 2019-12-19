527daac0447b9b1b42c825ba4ee3b17a14b8abe5
Matthew Boyle <matt.boyle@gmail.com>
2019-12-19 13:48:45 +0000

    the read_in_year method is no longer used

--- reading/series.py
+++ reading/series.py
@@ -185,11 +185,6 @@ class Series():
         else:
             return None
 
-    # returns the number of books from the series (being) read in $year
-    def read_in_year(self, year):
-        return len(self.df[self.df.Read.dt.year == year]) \
-             + len(self.df[self.df.Shelf == 'currently-reading'])
-
     # sort the books according to preference
     def sort(self):
         if self.order == 'series':
