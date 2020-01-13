f8b2372903b230b70672dbf9c7619bb17712acd8
Matthew Boyle <matt.boyle@gmail.com>
2020-01-13 20:20:56 +0000

    switch tests away from nose

--- t/test_reading_series.py
+++ t/test_reading_series.py
@@ -1,7 +1,6 @@
 # vim: ts=4 : sw=4 : et
 
 import pytest
-from nose.tools import eq_
 
 import pandas as pd
 
@@ -12,75 +11,75 @@ from reading.series import Series
 
 
 def test__get_entry():
-    eq_(_get_entry('1'), 1)
-    eq_(_get_entry('3'), 3)
-    eq_(_get_entry('1.1'), None)
-    eq_(_get_entry('1 of 2'), 1)
+    assert _get_entry('1') == 1
+    assert _get_entry('3') == 3
+    assert _get_entry('1.1') is None
+    assert _get_entry('1 of 2') == 1
 
 
 def test__parse_entries():
-    eq_(_parse_entries('1'), [1])
-    eq_(_parse_entries('1-2'), [1, 2])
-    eq_(_parse_entries('2-4'), [2, 3, 4])
-    eq_(_parse_entries('2-4 '), [2, 3, 4])
-    eq_(_parse_entries('0'), [0])
-    eq_(_parse_entries('0-2'), [0, 1, 2])
+    assert _parse_entries('1') == [1]
+    assert _parse_entries('1-2') == [1, 2]
+    assert _parse_entries('2-4') == [2, 3, 4]
+    assert _parse_entries('2-4 ') == [2, 3, 4]
+    assert _parse_entries('0') == [0]
+    assert _parse_entries('0-2') == [0, 1, 2]
 
-    eq_(_parse_entries('1, 2'), [1, 2])
-    eq_(_parse_entries('1,2'), [1, 2])
-    eq_(_parse_entries('1 & 2'), [1, 2])
-    eq_(_parse_entries('1&2'), [1, 2])
-    eq_(_parse_entries('1 & 3'), [1, 3])
-    eq_(_parse_entries('1, 2 & 4'), [1, 2, 4])
-    eq_(_parse_entries('1, 2, 4'), [1, 2, 4])
-    eq_(_parse_entries('1-3 , 5'), [1, 2, 3, 5])
-    eq_(_parse_entries('1-3 & 5'), [1, 2, 3, 5])
-    eq_(_parse_entries('1-4, 6-7'), [1, 2, 3, 4, 6, 7])
+    assert _parse_entries('1, 2') == [1, 2]
+    assert _parse_entries('1,2') == [1, 2]
+    assert _parse_entries('1 & 2') == [1, 2]
+    assert _parse_entries('1&2') == [1, 2]
+    assert _parse_entries('1 & 3') == [1, 3]
+    assert _parse_entries('1, 2 & 4') == [1, 2, 4]
+    assert _parse_entries('1, 2, 4') == [1, 2, 4]
+    assert _parse_entries('1-3 , 5') == [1, 2, 3, 5]
+    assert _parse_entries('1-3 & 5') == [1, 2, 3, 5]
+    assert _parse_entries('1-4, 6-7') == [1, 2, 3, 4, 6, 7]
 
-    eq_(_parse_entries(None), [])
-    eq_(_parse_entries(''), [])
-    eq_(_parse_entries(123), [])
-    eq_(_parse_entries(1.3), [])
+    assert _parse_entries(None) == []
+    assert _parse_entries('') == []
+    assert _parse_entries(123) == []
+    assert _parse_entries(1.3) == []
 
     # extra cruft
-    eq_(_parse_entries('1-3 omnibus'), [1, 2, 3])
-    eq_(_parse_entries('1 part 1'), [1])
-    eq_(_parse_entries('1.3 (Monarch of the Glen)'), [])
-    eq_(_parse_entries('1 of 2'), [1])
-    eq_(_parse_entries('2 of 2'), [2])
-    eq_(_parse_entries('I'), [])
-    eq_(_parse_entries('3 pt. 2'), [3])
-    eq_(_parse_entries('11B'), [11])
-    eq_(_parse_entries('1, part 2 of 2'), [1])
-    eq_(_parse_entries('2 (1/2)'), [2])
-    eq_(_parse_entries('3 part 2/2'), [3])
-    eq_(_parse_entries('Short Stories'), [])
+    assert _parse_entries('1-3 omnibus') == [1, 2, 3]
+    assert _parse_entries('1 part 1') == [1]
+    assert _parse_entries('1.3 (Monarch of the Glen)') == []
+    assert _parse_entries('1 of 2') == [1]
+    assert _parse_entries('2 of 2') == [2]
+    assert _parse_entries('I') == []
+    assert _parse_entries('3 pt. 2') == [3]
+    assert _parse_entries('11B') == [11]
+    assert _parse_entries('1, part 2 of 2') == [1]
+    assert _parse_entries('2 (1/2)') == [2]
+    assert _parse_entries('3 part 2/2') == [3]
+    assert _parse_entries('Short Stories') == []
 
     # dotted entries
-    eq_(_parse_entries('0.5'), [])
-    eq_(_parse_entries('0.5, 0.6'), [])
-    eq_(_parse_entries('0.5-0.6'), [])
-    eq_(_parse_entries('1-3, 3.1'), [1, 2, 3])
-    eq_(_parse_entries('4, 5.2 & 13 '), [4, 13])
+    assert _parse_entries('0.5') == []
+    assert _parse_entries('0.5, 0.6') == []
+    assert _parse_entries('0.5-0.6') == []
+    assert _parse_entries('1-3, 3.1') == [1, 2, 3]
+    assert _parse_entries('4, 5.2 & 13 ') == [4, 13]
 
 
 def test_interesting():
-    eq_(interesting('1', {
+    assert interesting('1', {
         'Entries': ['1', '2', '3']
-    }), True)
+    })
 
-    eq_(interesting('1', {
+    assert not interesting('1', {
         'Entries': ['1']
-    }), False)
+    })
 
-    eq_(interesting('1-2', {
+    assert not interesting('1-2', {
         'Entries': ['1', '2']
-    }), False)
+    })
 
 
 def test_ignore():
-    eq_(reading.series.ignore(1), False)
-    eq_(reading.series.ignore(55486), True)
+    assert not reading.series.ignore(1)
+    assert reading.series.ignore(55486)
 
 
 ################################################################################
