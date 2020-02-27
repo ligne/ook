0f5fc85a82b6c1d0963e05000b178dd366d4a141
Matthew Boyle <matt.boyle@gmail.com>
2020-02-27 18:57:55 +0000

    docstrings for storage.py

--- reading/storage.py
+++ reading/storage.py
@@ -1,5 +1,7 @@
 # vim: ts=4 : sw=4 : et
 
+"""Functions for loading and storing dataframes as csv."""
+
 import pandas as pd
 
 from .config import df_columns, date_columns
@@ -13,6 +15,7 @@ def _load_csv(name, columns, parse_dates=False):
 
 
 def load_df(name, fname=None):
+    """Load and return a dataframe of type $name, creating it if necessary."""
     return _load_csv(
         fname or f"data/{name}.csv",
         columns=df_columns(name),
@@ -21,6 +24,7 @@ def load_df(name, fname=None):
 
 
 def save_df(name, df, fname=None):
+    """Save a dataframe of type $name in an aesthetic format."""
     df.sort_index().to_csv(
         fname or f"data/{name}.csv",
         columns=df_columns(name),
