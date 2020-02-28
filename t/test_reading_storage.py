2db000ad527a1e6fd7a23c190f6f740e31440366
Matthew Boyle <matt.boyle@gmail.com>
2020-02-28 18:44:26 +0000

    tests for save_df()

--- t/test_reading_storage.py
+++ t/test_reading_storage.py
@@ -1,6 +1,7 @@
 # vim: ts=4 : sw=4 : et
 
-from reading.storage import load_df
+from reading.collection import Collection
+from reading.storage import load_df, save_df
 
 
 def test_load_df():
@@ -9,3 +10,33 @@ def test_load_df():
 
     df = load_df("authors", fname="/does/not/exist")
     assert df.empty, "Loaded a dataframe from a missing file"
+
+
+def test_save_df(tmp_path):
+    df = Collection(gr_csv="t/data/goodreads-2019-12-04.csv", fixes=False, metadata=False).df
+
+    # pick out a few books
+    df = df[df.AuthorId == 9121]
+
+    sorted_csv = tmp_path / "ebooks.csv"
+    save_df("ebooks", df, sorted_csv)
+    assert sorted_csv.read_text() == """\
+BookId,Author,Title,Category,Language,Words,Added
+38290,James Fenimore Cooper,The Pioneers,novels,,,2017-02-27
+246245,James Fenimore Cooper,The Deerslayer,novels,en,,2016-11-08
+347245,James Fenimore Cooper,The Pathfinder,novels,en,,2016-11-08
+621017,James Fenimore Cooper,The Prairie,novels,,,2016-11-08
+1041744,James Fenimore Cooper,The Last of the Mohicans,novels,,,2017-02-16
+""", "Wrote a csv of only some columns"
+
+    shuffled_df = df[sorted(df.columns)].sample(frac=1)
+    shuffled_csv = tmp_path / "shuffled.csv"
+    save_df("ebooks", shuffled_df, shuffled_csv)
+    assert shuffled_csv.read_text() == """\
+BookId,Author,Title,Category,Language,Words,Added
+38290,James Fenimore Cooper,The Pioneers,novels,,,2017-02-27
+246245,James Fenimore Cooper,The Deerslayer,novels,en,,2016-11-08
+347245,James Fenimore Cooper,The Pathfinder,novels,en,,2016-11-08
+621017,James Fenimore Cooper,The Prairie,novels,,,2016-11-08
+1041744,James Fenimore Cooper,The Last of the Mohicans,novels,,,2017-02-16
+""", "CSV is ordered even if the df isn't"
