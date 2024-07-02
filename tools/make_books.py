2ffeb141538c260245c59c56e5da1274ff71bd48
Matt Boyle <matt.boyle@gmail.com>
2024-07-02 21:49:17 +0100

    class to represent a book

--- tools/make_books.py
+++ tools/make_books.py
@@ -5,6 +5,42 @@
 from __future__ import annotations
 
 import argparse
+import datetime as dt
+
+from attrs import define
+
+
+###############################################################################
+
+
+@define
+class Book:
+    """Represent a book."""
+
+    BookId: int
+    Author: str
+    AuthorId: int
+    Title: str
+    Work: int
+    Shelf: str
+    Category: str | None
+    Scheduled: dt.date | None
+    Borrowed: bool
+    Series: str | None
+    SeriesId: str | None
+    Entry: str | int | None
+    Binding: str | None
+    Published: int | None
+    Language: str | None
+    Pages: int | None
+    Added: dt.date
+    Started: dt.date | None
+    Read: dt.date | None
+    Rating: int | None
+    Words: int | None
+    Gender: str | None
+    Nationality: str | None
+
 
 ###############################################################################
 
